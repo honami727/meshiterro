af2fb171a97caed774dfcb483d520697aa289c46
honami727 <honami727mogul@gmail.com>
2024-04-06 16:31:08 +0000

    投稿機能作成10章途中

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,17 @@ class HomesController < ApplicationController
     
     def top
     end
+    
+    def about
+    end
+    
+    def new
+    end
+    
+    def index
+    end
+    
+    def show
+    end
+    
 end
